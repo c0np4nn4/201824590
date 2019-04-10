b37d6899957b0c0de6742c904e7092a1b50b874f
c0np4nn4 <c0np4nn4@gmail.com>
2019-04-10 08:31:28 -0700

    First commit

--- hello.c
+++ hello.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main() {
-	printf("hello, Open Source Programming\n");
+int main(void) {
+	printf("hello, Open Source Programming!\n");
 	return 0;
 }
--- hello2.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-	printf("This is for practice\n");
-	return 0;
-}
